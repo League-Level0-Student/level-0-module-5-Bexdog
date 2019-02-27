c9961211a9553ef080e46acce9970692125d3660
sepfish <40220408+sepfish@users.noreply.github.com>
2019-02-26 19:33:11 -0800

    goofy Names, guessing pi, circle ring

--- src/algorithms/GoofyNames.java
+++ src/algorithms/GoofyNames.java
@@ -9,7 +9,7 @@ import javax.swing.JOptionPane;
 public class GoofyNames {
 	public static void main(String[] args) {
 		String goofyName = "";
-
+String Bob="";
 		// 1. Ask the user to enter their name
 String name = JOptionPane.showInputDialog("I am a stalker, what is your name");
 		// 2. Print upper case name to the console using .toUpperCase()
@@ -18,20 +18,24 @@ String Name = name.toUpperCase();
 System.out.println(Name);
 		// 3. Loop through each character of the name (steps 4 - 6).
 		//    HINT: Use .length() to determine the number of characters in the String.
-
+for(int i = 0; i<Name.length();i++) {
 			// 4. Create a String variable to store the next character of the name
 			//    using .substring(start, end)
 			//    HINT: replace 'start' and 'end' to get String with character at i 
-			
+		
 			// 5. Use MODULO to set this variable to upper case for EVEN characters
 			//    and lower case for ODD characters.
-
-			
+if(i%2==1){
+	Bob = Bob.concat(Name.substring(i,i+1).toLowerCase());
+}
+else {
+	Bob = Bob.concat(Name.substring(i,i+1));
+}
 			// 6. ADD this String  (containing 1 char) to the goofyName String
 
-		
+}
 		// 7. Use pop-up to show user their Goofy name
-
+JOptionPane.showMessageDialog(null, Bob);
 	}
 }
 
