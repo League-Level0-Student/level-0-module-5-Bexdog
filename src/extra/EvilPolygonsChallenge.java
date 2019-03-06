0c9df343cd7e54bc80795a6c6a26499066f225c3
sepfish <40220408+sepfish@users.noreply.github.com>
2019-03-05 20:01:07 -0800

    Code 4 life, Polygon Challange, for loop gauntlet

--- src/extra/EvilPolygonsChallenge.java
+++ src/extra/EvilPolygonsChallenge.java
@@ -6,24 +6,58 @@ import javax.swing.JOptionPane;
 import org.jointheleague.graphical.robot.Robot;
 
 public class EvilPolygonsChallenge {
-	
+	static Robot bob = new Robot();
+	static int Sides;
 	public static void main(String[] args) {
 		
 		//1. Create a new Robot
 
 		//2. Set the speed to 100
-
-		int colorChoice=JOptionPane.showOptionDialog(null, "hello", "title", JOptionPane.INFORMATION_MESSAGE, JOptionPane.DEFAULT_OPTION, null, new String[]{"Color 1","Color 2", "Color 3"}, 0);
-		
+bob.setSpeed(100);
+bob.setY(500);
+		int colorChoice=JOptionPane.showOptionDialog(null, "What color do you want your polygon to be?", "title", JOptionPane.INFORMATION_MESSAGE, JOptionPane.DEFAULT_OPTION, null, new String[]{"Red","Blue", "Green", "Black", "Orange", "Yellow"}, 0);
+		String sides=JOptionPane.showInputDialog("How many sides do your want the polygon to have?");
+		Sides = Integer.parseInt(sides);
 		//3. Use if statements to check the the value of colorChoice and set the pen color accordingly 
-		
+	if(colorChoice==0) {
+		bob.setPenColor(Color.red);
+	}	
+	else if(colorChoice==1) {
+		bob.setPenColor(Color.blue);
+	}	
+	else if(colorChoice==2){
+		bob.setPenColor(Color.green);
+	}	
+	else if(colorChoice==3){
+		bob.setPenColor(Color.black);
+	}	
+	else if(colorChoice==4){
+		bob.setPenColor(Color.orange);
+	}	
+	else if(colorChoice==5){
+		bob.setPenColor(Color.yellow);
+	}	
 		//4. Ask the use how many polygons they want to be drawn.
-		
-		//5. Use the robot to draw the number of polygons the user requested.
-		
+	String number = JOptionPane.showInputDialog("how many polygons do you want to be drawn?");	
+	int Number = Integer.parseInt(number);	
+	//5. Use the robot to draw the number of polygons the user requested.
+		for(int i =0;i<Number;i++) {
+			drawSquare();
+		}
 		//6. Make it so your shapes do not overlap
 
-		//7. Challenge: add more colors to the Option Dialog.
 	}
-}
+		//7. Challenge: add more colors to the Option Dialog.
+	static void drawSquare() {
+		bob.penDown();
+		for(int i =0;i<Sides;i++) {
+		bob.move(70);
+		bob.turn(360/Sides); 
+		}
+		bob.penUp();
+		bob.move(200);
+	}
+	
+	}
+
 
--- src/nested_loops/For_loop_gauntlet.java
+++ src/nested_loops/For_loop_gauntlet.java
@@ -2,47 +2,85 @@ package nested_loops;
 
 public class For_loop_gauntlet {
 public static void main(String[] args) {
-	
-	for(int i=0;i<101;i++) {
-	System.out.println(i);
-	}
-	for(int i=100;i>-1;i--) {
-		System.out.println(i);
-		}
-	for(int i=2;i<100;i++) {
-		if(i%2==0) {
-		System.out.println(i);
-		}
-		}
-	for(int i=2;i<100;i++) {
-		if(i%2==1) {
-		System.out.println(i);
-		}
-		}
-	for(int i=0;i<501;i++) {
-		if(i%2==0) {
-		System.out.print(i);
-		System.out.println(" even");
-		}
-		if(i%2==1) {
-			System.out.print(i);
-			System.out.println(" odd");
-			}
-		}
-	for(int i=0;i<778;i++) {
-		if(i%7==0) {
-		System.out.println(i);
-		}
-		}
-	for(int i=2007;i<2020;i++) {
-		 {
-		System.out.print("In "+i+", ");
-		System.out.println("I was " + (i-2007));
-		}
-	
-	
-	
 
-	
-	}}}
+	int one = 0;
+	int two=0;
+//	for(int i=0;i<101;i++) {
+//	System.out.println(i);
+//	}
+//	for(int i=100;i>-1;i--) {
+//		System.out.println(i);
+//		}
+//	for(int i=2;i<100;i++) {
+//		if(i%2==0) {
+//		System.out.println(i);
+//		}
+//		}
+//	for(int i=2;i<100;i++) {
+//		if(i%2==1) {
+//		System.out.println(i);
+//		}
+//		}
+//	for(int i=0;i<501;i++) {
+//		if(i%2==0) {
+//		System.out.print(i);
+//		System.out.println(" even");
+//		}
+//		if(i%2==1) {
+//			System.out.print(i);
+//			System.out.println(" odd");
+//			}
+//		}
+//	for(int i=0;i<778;i++) {
+//		if(i%7==0) {
+//		System.out.println(i);
+//		}
+//		}
+//	for(int i=2007;i<2020;i++) {
+//		 {
+//		System.out.print("In "+i+", ");
+//		System.out.println("I was " + (i-2007));
+//		}
+//	for(int b = 0; b<9;b++) { this was another way for number 1
+//	System.out.print(one);
+//	System.out.println(two);
+//	two++;
+//	if(two==3) {
+//	two=0;
+//	one++;
+//	}
+//	}    
+//for(int i =0;i<3;i++) {
+//	for(int j=0;j<3;j++) {
+//	System.out.println(i+" "+j);
+//	}
+//}
+//	for(int i=0;i<3;i++) {
+//	for(int j=1;j<4;j++) {
+//		System.out.print(j+(i*3)+" ");
+//		
+//	}
+//	System.out.println("");
+//	}
+//	for(int i=0;i<10;i++) {
+//	for(int j=1;j<11;j++) {
+//			System.out.print(j+(i*10)+" ");
+//			
+//		}
+//	System.out.println("");
+//	}
+//	for(int i=1;i<7;i++) {
+//		for(int j=0;j<i;j++) {
+//			System.out.print("* ");
+//			
+//		}
+//		System.out.println("");
+//		}
+	for(int i=0;i<101;i++) {
+		System.out.println(100-i);
+		}
+}
+//what are the bobs?  bob,boB,bOb,Bob,bOB,BOb,BoB,BOB	
+}
+
 
